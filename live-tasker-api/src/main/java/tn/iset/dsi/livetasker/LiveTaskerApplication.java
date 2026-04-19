1d3d3f1c2a0b44fcbc2cacee2badb8f27026c10a
Adem-elwafi <adem.elwafi.dev@gmail.com>
2026-04-19 19:21:19 +0100

    feat: initialize Spring Boot 3.2 project with Java 21 and Virtual Threads configuration

---
+++ live-tasker-api/src/main/java/tn/iset/dsi/livetasker/LiveTaskerApplication.java
@@ -0,0 +1,13 @@
+package tn.iset.dsi.livetasker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiveTaskerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiveTaskerApplication.class, args);
+	}
+
+}
---
+++ live-tasker-api/src/test/java/tn/iset/dsi/livetasker/LiveTaskerApplicationTests.java
@@ -0,0 +1,13 @@
+package tn.iset.dsi.livetasker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiveTaskerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
